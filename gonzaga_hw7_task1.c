5fe3d728ce2cfa1040f2ca7e5a152b6d537b0314
Chris Werner <chris.lex.werner@gmail.com>
2017-03-17 03:11:22 +0000

    initial commit of task1

---
+++ gonzaga_hw7_task1.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  gonzaga_hw7_task1.c
+ *       	Usage:  ./gonzaga_hw7_task1.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/17/2017 03:10:59 AM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Chris Werner (), christopherwerner@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>		/* For Standard Library*/
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
